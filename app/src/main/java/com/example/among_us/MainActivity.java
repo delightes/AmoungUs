bea51ea4b848c0582f1257e67e122c2674cb201f
yoonui <yoony0612@ajou.ac.kr>
2021-02-14 00:59:44 +0900

    2021-02-13 1. 처음 시작 화면 추가 + 가로모드 2. 시작화면에 play 버튼과 how to play 버튼 추가 3. play 버튼 누르면 다음 화면으로 넘어가는 기능 구현 4. 배경음악 세팅

---
+++ app/src/main/java/com/example/among_us/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.among_us;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View decorView;
+    private int	uiOption;
+    private static MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 상태바 제거
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 하단바 제거
+        decorView = getWindow().getDecorView();
+        uiOption = getWindow().getDecorView().getSystemUiVisibility();
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH )
+            uiOption |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN )
+            uiOption |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT )
+            uiOption |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility( uiOption );
+
+        // 배경음악
+        mp = MediaPlayer.create(this, R.raw.main_bgm);
+        mp.setLooping(true);
+
+        setContentView(R.layout.activity_main);
+        mp.start();
+    }
+
+    public void play_onclick(View view) {
+        Intent intent = new Intent(getApplicationContext(), second.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/among_us/second.java
@@ -0,0 +1,14 @@
+package com.example.among_us;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
